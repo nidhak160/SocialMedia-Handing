a83e4fe160f6ed2e8286a7be6d6787c194736fe2
Nidha <your_email@example.com>
2026-07-27 16:43:59 +0530

    Clean up requirements.txt for deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'yourapp.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://socialmedia-handing-1.onrender.com']
 
 
 # Application definition
